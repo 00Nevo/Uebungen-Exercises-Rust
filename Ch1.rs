8d89dba352a0761bd5bc4baa4ecab05493b79b3b
NEVO <albertoborgato9@gmail.com>
2025-03-31 00:12:59 +0200

    Update Ch1.rs

--- Ch1.rs
+++ Ch1.rs
@@ -13,6 +13,20 @@ Starting the Install on Other Operating Systems
 Finishing the Installation
 Verifying Your Rust Installation
 Testing Rust with “Hello, World”
+
+C:\Users\newuser>cd .cargo
+C:\Users\newuser\.cargo>cargo new testrust
+    Creating binary (application) `testrust` package
+note: see more `Cargo.toml` keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+C:\Users\newuser\.cargo>cd testrust
+C:\Users\newuser\.cargo\testrust>cargo run
+   Compiling testrust v0.1.0 (C:\Users\newuser\.cargo\testrust)
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 2.94s
+     Running `target\debug\testrust.exe`
+Hello, world!
+C:\Users\newuser\.cargo\testrust>
+  
 Updating Rust
 Installing and Configuring Your Development
 Environment
