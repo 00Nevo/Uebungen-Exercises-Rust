8003c2ce5d8f8b4917bad951fd4ac51b8d70314e
NEVO <albertoborgato9@gmail.com>
2025-03-30 23:53:13 +0200

    Create Ch1.rs

---
+++ Ch1.rs
@@ -0,0 +1,26 @@
+Hands-on Rust
+Effective Learning through 2D Game Development and Play
+Herbert Wolverson
+
+Part I
+Getting Started with Rust
+
+CHAPTER 1
+Rust and Your Development Environment
+Installing Rust
+Starting the Install on Microsoft Windows
+Starting the Install on Other Operating Systems
+Finishing the Installation
+Verifying Your Rust Installation
+Testing Rust with “Hello, World”
+Updating Rust
+Installing and Configuring Your Development
+Environment
+Managing Projects with Cargo
+Selecting a “Home” Directory for Your Code
+Starting a New Project with Cargo
+Run Hello, World
+Creating Your First Rust Program
+Cargo Metadata
+Hello, World
+Using Cargo to Build, Check, or Run Your Project .. 
